e61ac7bd9e70aa20c3d4ca2c3e70b4be14f40a3b
Leo Driesch <leo.driesch@gmail.com>
2020-06-03 22:19:09 +0200

    :label: Convert files to typescript

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
+import { AppProps } from 'next/app';
 import Head from 'next/head';
 import '../styles/index.css';
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps }: AppProps) {
     return (
         <>
             <Head>
