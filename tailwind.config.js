d4496a5cb91695c3a6623a0046ab76765d3e15c3
Leonhard Driesch <leo.driesch@gmail.com>
2020-06-03 17:54:52 +0200

    :wrench: Set up tailwind and postcss configuration files

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    purge: ['./pages/**/*.*', './components/**/*.*'],
+    theme: {
+        extend: {},
+        fontFamily: {
+            body: ['Inter', 'sans-serif'],
+        },
+    },
+    variants: {},
+    plugins: [],
+};
