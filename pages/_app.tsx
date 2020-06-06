b09eb708f8ee2bd10e796f76d0d3c980c0637f6f
Leo Driesch <leo.driesch@gmail.com>
2020-06-06 13:49:03 +0200

    :wrench: Change source of inter font files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,6 @@
 import { AppProps } from 'next/app';
-import Head from 'next/head';
 import '../styles/index.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-    return (
-        <>
-            <Head>
-                <link
-                    href="https://fonts.googleapis.com/css2?family=Inter:wght@531&display=swap"
-                    rel="stylesheet"
-                />
-            </Head>
-            <div className="font-body">
-                <Component {...pageProps} />
-            </div>
-        </>
-    );
+    return <Component {...pageProps} />;
 }
