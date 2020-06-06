bbe0cd365c89c58bdbbe5eadec2bf65146667243
Leo Driesch <leo.driesch@gmail.com>
2020-06-06 13:49:19 +0200

    :fire: Remove unused font definition from tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,6 @@ module.exports = {
     purge: ['./pages/**/*.*', './components/**/*.*'],
     theme: {
         extend: {},
-        fontFamily: {
-            body: ['Inter', 'sans-serif'],
-        },
     },
     variants: {},
     plugins: [require('@tailwindcss/ui')],
